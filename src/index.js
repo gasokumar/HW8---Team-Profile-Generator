693e5eb1be1faaa86eab1816c4d1b2984a08c180
Ami Asokumar <gasokumar@gmail.com>
2021-11-08 17:27:54 -0500

    added pseudocode for src/index.js, which is comprised of HTML template helper code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Template helper code
+
+//Create functions that will create an html template/card for each profile
+
+//Create a function that makes an array that will store the html templates once they've been created
+
+//Join the array into a string and push it to the html page.
+
+//Export to index
