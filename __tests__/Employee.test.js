bbbbfe4ae47b9e47fe5474bbb85065e192028a72
Ami Asokumar <gasokumar@gmail.com>
2021-11-08 04:44:26 -0500

    Finished writing employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,27 +5,27 @@ describe("Employee", () => {
     it("should return an object with name, id, and email properties", () => {
       const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
 
-      expect("name" in employee).toEqual("Ami");
-      expect("id" in employee).toEqual(22);
-      expect("email" in employee).toEqual("gasokumar@gmail.com");
+      expect(employee.name).toEqual("Ami");
+      expect(employee.id).toEqual(313);
+      expect(employee.email).toEqual("gasokumar@gmail.com");
     });
   });
   describe("Methods", () => {
     it("should get the employee's name from getName()", () => {
       const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
-      expect(employee.getName).toEqual("Ami");
+      expect(employee.getName()).toEqual("Ami");
     });
     it("should get the employee's id from getId()", () => {
       const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
-      expect(employee.getId).toEqual(313);
+      expect(employee.getId()).toEqual(313);
     });
     it("should get the employee's email from getEmail()", () => {
       const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
-      expect(employee.getEmail).toEqual("gasokumar@gmail.com");
+      expect(employee.getEmail()).toEqual("gasokumar@gmail.com");
     });
     it("should get the employee's role from getRole()", () => {
       const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
-      expect(employee.getRole).toEqual("Employee");
+      expect(employee.getRole()).toEqual("Employee");
     });
   });
 });
