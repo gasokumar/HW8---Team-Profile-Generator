4cc4483099783415f901282e11b46f9c447420ac
Ami Asokumar <gasokumar@gmail.com>
2021-11-08 18:30:18 -0500

    added more pseudocode and imported node modules for index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+//This file runs the application. Has inquirer prompts, stores data, then calls the template helper code functions to create html. Then writes to the index file.
+
+//Import fs and inquirer prompts
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+//Write inquirer prompts
+
+//Store data in an array and use functions that take in that data to create the cards/html.
+
+//Will probably need promises for inquire prompts, then use that data to write to the file.
---
+++ src/templateHelperCode.js
@@ -0,0 +1,10 @@
+//Template helper code --> Will be responsible for housing functions that create the html for employee cards, joining it together into an HTML document/string and then exporting it to the actual index.html file.
+// Basically, is responsible for creating the HTML
+
+//Create functions that will create an html template/card for each profile
+
+//Create a function that makes an array that will store the html templates once they've been created
+
+//Join the array into a string and push it to the html page.
+
+//Export to index
