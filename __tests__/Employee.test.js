b8859fd4e5d18ea54c2863592560c859a566dfab
Ami Asokumar <gasokumar@gmail.com>
2021-11-08 04:30:35 -0500

    created test for employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,20 +1,31 @@
-class Employee {
-  constructor(name, id, email) {
-    this.name = name;
-    this.id = id;
-    this.email = email;
-  }
-  getName() {
-    return this.name;
-  }
-  getId() {
-    return this.id;
-  }
-  getEmail() {
-    return this.email;
-  }
-  getRole() {
-    return "Employee";
-  }
-}
-module.exports = Employee;
+const Employee = require("../lib/Employee.js");
+
+describe("Employee", () => {
+  describe("Initialization", () => {
+    it("should return an object with name, id, and email properties", () => {
+      const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
+
+      expect("name" in employee).toEqual("Ami");
+      expect("id" in employee).toEqual(22);
+      expect("email" in employee).toEqual("gasokumar@gmail.com");
+    });
+  });
+  describe("Methods", () => {
+    it("should get the employee's name from getName()", () => {
+      const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
+      expect(employee.getName).toEqual("Ami");
+    });
+    it("should get the employee's id from getId()", () => {
+      const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
+      expect(employee.getId).toEqual(313);
+    });
+    it("should get the employee's email from getEmail()", () => {
+      const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
+      expect(employee.getEmail).toEqual("gasokumar@gmail.com");
+    });
+    it("should get the employee's role from getRole()", () => {
+      const employee = new Employee("Ami", 313, "gasokumar@gmail.com");
+      expect(employee.getRole).toEqual("Employee");
+    });
+  });
+});
