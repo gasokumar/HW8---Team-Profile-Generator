8d63807a7759c5eeccf3ad0ea005ac52a138f407
Ami Asokumar <gasokumar@gmail.com>
2021-11-08 22:03:15 -0500

    finished adding manager inputs

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 //This file actually runs the application. Has inquirer prompts, stores data, then calls the template helper code functions to create html. Then writes to the index file. This file does the heavy lifting/uses all the stuff we've created. If all the files/functions we created were the infinity stones, then this file is Thanos with the gauntlet.
 
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 
@@ -18,7 +15,11 @@
 //Import fs and inquirer prompts
 const fs = require("fs");
 const inquirer = require("inquirer");
+const { off } = require("process");
+const Manager = require("./lib (Classes)/Manager");
 
+// Array to put team members in as they are generated
+teamArray = [];
 //Write inquirer prompts
 //Manager prompts for when app starts up
 //team manager’s name, employee ID, email address, and office number
@@ -43,8 +44,14 @@ inquirer
       message: "What is the manager's office number?",
     },
   ])
-  .then((data) => {
-    console.log(data);
+  .then((input) => {
+    const { name, id, email, officeNumber } = input;
+    //Object destructuring: {property, property, property, property} = object
+    const manager = new Manager(name, id, email, officeNumber);
+    console.log(manager);
+    teamArray.push(manager);
+
+    // const manager = new Manager();
   });
 // };
 
